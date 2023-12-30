d1d9dc5d99abd909599b695915e08089a4af49ff
Augusto Souza <augustosouzas01@gmail.com>
2023-12-29 23:20:58 -0300

    Creating Spring Boot REST API

---
+++ SistemaDeTransferencia/src/main/java/com/AugustoSouza/SistemaDeTransferencia/SistemaDeTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.AugustoSouza.SistemaDeTransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTransferenciaApplication.class, args);
+	}
+
+}
